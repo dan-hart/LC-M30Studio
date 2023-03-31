f2ecbe3131b261752b338d5c0764a4f2cddcaef0
Dan Hart <dan-hart@users.noreply.github.com>
2023-03-30 20:19:14 -0500

    M30 Studio

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    // write a program to count the number of times each character occurs in a string and then print the results to the console.
+    public static void countCharacters(String str) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        System.out.println(map);
+    }
+}
---
+++ src/MainTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @org.junit.jupiter.api.Test
+    void countCharacters() {
+        Main.countCharacters("Hello world!");
+    }
+}
